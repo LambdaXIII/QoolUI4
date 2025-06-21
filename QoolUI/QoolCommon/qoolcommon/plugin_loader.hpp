d2f4d0127afb443ae7259d99bd06de7204eddd7e
xiii_1991 <xiii_1991@163.com>
2025-06-21 20:39:09 +0800

    ported old helpers

---
+++ QoolUI/QoolCommon/qoolcommon/plugin_loader.hpp
@@ -0,0 +1,149 @@
+#ifndef QOOLCOMMON_PLUGIN_LOADER_HPP
+#define QOOLCOMMON_PLUGIN_LOADER_HPP
+
+#include "qoolns.hpp"
+
+#include <QAnyStringView>
+#include <QCoreApplication>
+#include <QDirIterator>
+#include <QFileInfo>
+#include <QLibrary>
+#include <QPluginLoader>
+#include <QSet>
+#include <QString>
+#include <QStringList>
+
+QOOL_NS_BEGIN
+
+class PluginScanner {
+  QStringList m_subfolders;
+  bool m_defaultPathEnabled { true };
+  QStringList m_extraFolders;
+
+public:
+  PluginScanner() = default;
+  explicit PluginScanner(QAnyStringView subfoldere) {
+    m_subfolders << subfoldere.toString();
+  }
+  PluginScanner& addSubFolder(QAnyStringView f) {
+    m_subfolders << f.toString();
+    return *this;
+  }
+
+  PluginScanner& addExtraPath(const QString& p) {
+    m_extraFolders << p;
+    return *this;
+  }
+  PluginScanner& set_defaultPathEnabled(bool v) {
+    m_defaultPathEnabled = v;
+    return *this;
+  }
+  QStringList pluginPaths() const {
+    QStringList result;
+    QSet<QString> rawPaths;
+
+    if (m_defaultPathEnabled) {
+      for (const auto& libPath : QCoreApplication::libraryPaths())
+        rawPaths << libPath;
+      rawPaths << QCoreApplication::applicationDirPath();
+    }
+
+    for (const auto& extraPath : m_extraFolders)
+      rawPaths << extraPath;
+
+    if (m_subfolders.isEmpty())
+      result = QStringList(rawPaths.constBegin(), rawPaths.constEnd());
+    else
+      for (const auto& sub : m_subfolders) {
+        std::for_each(
+          rawPaths.cbegin(), rawPaths.cend(), [&](const QString& x) {
+            auto n = x + '/' + sub;
+            result << n;
+          });
+      }
+
+    result.removeIf([](const QString& x) {
+      QFileInfo p(x);
+      bool good = p.exists() && p.isDir();
+      return ! good;
+    });
+    std::sort(result.begin(), result.end());
+    auto last = std::unique(result.begin(), result.end());
+    result.erase(last, result.end());
+    result.squeeze();
+    return result;
+  }
+
+  QStringList plugins() const {
+    QStringList result;
+    for (const QString& dir : pluginPaths()) {
+      QDirIterator iter(dir);
+      while (iter.hasNext()) {
+        auto a = iter.next();
+        if (QLibrary::isLibrary(a))
+          result << a;
+      }
+    }
+    return result;
+  }
+}; // pluginscanner
+
+inline QString pluginMetadata(QPluginLoader& loader, QAnyStringView key,
+  const QString& defaultValue = "") {
+  return loader.metaData()
+    .value("MetaData")
+    .toObject()
+    .value(key.toString())
+    .toString(defaultValue);
+}
+
+class PriorityFixer {
+  qreal m_step { 0.01 };
+
+public:
+  PriorityFixer(qreal stepValue = 0.01)
+    : m_step { stepValue } {}
+  template <typename KeyContainer>
+  qreal operator()(int priority, const KeyContainer& keys) {
+    qreal p(priority);
+    while (std::find(keys.cbegin(), keys.cend(), p) != keys.cend())
+      p += m_step;
+    return p;
+  }
+};
+
+template <typename Interface>
+struct PluginLoader {
+  using InstanceMap = QMultiHash<QString, Interface*>;
+
+  static InstanceMap loadInstances() {
+    XIII_NS::PluginScanner scanner(QOOL_PLUGIN_DIR);
+    scanner.addExtraPath(QCoreApplication::applicationDirPath());
+    auto plugins = scanner.plugins();
+    if (plugins.isEmpty())
+      return {};
+
+    InstanceMap result;
+
+    for (const auto& pluginPath : plugins) {
+      QScopedPointer<QPluginLoader> loader { new QPluginLoader(
+        pluginPath) };
+      auto ins_object = loader->instance();
+
+      Interface* ins_interface = qobject_cast<Interface*>(ins_object);
+      if (ins_interface == nullptr) {
+        loader->unload();
+        continue;
+      }
+      QString plugin_name =
+        XIII_NS::pluginMetadata(*loader, "name", "");
+      result.insert(plugin_name, ins_interface);
+    }
+
+    return result;
+  }
+};
+
+QOOL_NS_END
+
+#endif // QOOLCOMMON_PLUGIN_LOADER_HPP
---
+++ QoolUI/QoolCommon/qoolcommon/singleton.hpp
@@ -0,0 +1,42 @@
+#ifndef QOOLCOMMON_SINGLETON_HPP
+#define QOOLCOMMON_SINGLETON_HPP
+
+#define QOOL_SIMPLE_SINGLETON_DECL(_CLS_)                              \
+private:                                                               \
+  static _CLS_* m_instance;                                            \
+  _CLS_();                                                             \
+                                                                       \
+public:                                                                \
+  static _CLS_* instance();
+
+#define XIII_SIMPLE_SINGLETON_STL_IMPL(_CLS_)                          \
+  _CLS_* _CLS_::m_instance { nullptr };                                \
+  _CLS_* _CLS_::instance() {                                           \
+    static std::mutex mutex;                                           \
+    if (m_instance == nullptr) {                                       \
+      std::lock_guard<std::mutex> locker(&mutex);                      \
+      if (m_instance == nullptr)                                       \
+        m_instance = new _CLS_;                                        \
+    }                                                                  \
+    return m_instance;                                                 \
+  }
+
+#define XIII_SIMPLE_SINGLETON_QT_IMPL(_CLS_)                           \
+  _CLS_* _CLS_::m_instance { nullptr };                                \
+  _CLS_* _CLS_::instance() {                                           \
+    static QMutex mutex;                                               \
+    if (m_instance == nullptr) {                                       \
+      QMutexLocker locker(&mutex);                                     \
+      if (m_instance == nullptr)                                       \
+        m_instance = new _CLS_;                                        \
+    }                                                                  \
+    return m_instance;                                                 \
+  }
+
+#define XIII_SIMPLE_SINGLETON_QML_CREATE(_CLS_)                        \
+public:                                                                \
+  static _CLS_* create(QQmlEngine*, QJSEngine*) {                      \
+    return _CLS_::instance();                                          \
+  }
+
+#endif // QOOLCOMMON_SINGLETON_HPP
