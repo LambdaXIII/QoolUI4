c18f65a972ff6bfd13bbd2fd62b7eb3b4b49c2dd
xiii_1991 <xiii_1991@163.com>
2025-06-24 16:24:29 +0800

    new debug macros

--- QoolUI/QoolCommon/qoolcommon/debug.hpp
+++ QoolUI/QoolCommon/qoolcommon/debug.hpp
@@ -1,6 +1,8 @@
 #ifndef QOOL_DEBUG_HPP
 #define QOOL_DEBUG_HPP
 
+#include <QMetaObject>
+#include <QMetaProperty>
 #include <QtDebug>
 
 #define xDBGReset "\033[0m"
@@ -73,5 +75,118 @@ inline QDebug operator<<(
 #define xWarningQ xWarning << xDBGToken(staticMetaObject.className())
 #define xCriticalQ xCritical << xDBGToken(staticMetaObject.className())
 #define xFatalQ xFatal << xDBGToken(staticMetaObject.className())
+
+struct __XDBGVARIANT__ {
+  const QVariant& variant;
+  inline QString valueString() const {
+    if (variant.isNull())
+      return "NULL";
+    if (variant.canConvert<QString>())
+      return variant.toString();
+    return QString("<%1>").arg(variant.typeName());
+  }
+  inline QString typeName() const {
+    auto result = QString::fromLatin1(variant.typeName());
+    if (result.isEmpty())
+      return "???";
+    return result;
+  }
+  __XDBGVARIANT__(const QVariant& x)
+    : variant(x) {}
+};
+inline QDebug operator<<(
+  QDebug debug, const __XDBGVARIANT__& t) noexcept {
+  debug.noquote().nospace()
+    << xDBGWhite << xDBGGreen << t.variant.typeName() << xDBGReset
+    << "(" << xDBGYellow << t.valueString() << xDBGReset << ")";
+  return debug.space();
+};
+#define xDBGVariant(__variant__) __XDBGVARIANT__(__variant__)
+
+template <typename K, typename V>
+struct __XDBGMAP__ {
+  const QMap<K, V>& map;
+  __XDBGMAP__(const QMap<K, V>& x)
+    : map(x) {}
+};
+
+template <typename K, typename V>
+inline QDebug operator<<(
+  QDebug debug, const __XDBGMAP__<K, V>& t) noexcept {
+  debug.noquote().nospace()
+    << xDBGBlue << "[QMap:" << xDBGReset << t.map.count() << xDBGBlue
+    << "]" << xDBGReset;
+
+  for (auto iter = t.map.constBegin(); iter != t.map.constEnd();
+    ++iter) {
+    debug.noquote().nospace()
+      << "\n  " << xDBGYellow << iter.key() << xDBGReset << "\t\t:\t"
+      << xDBGGreen << iter.value() << xDBGReset;
+  }
+  return debug.space();
+}
+
+template <>
+inline QDebug operator<<(
+  QDebug debug, const __XDBGMAP__<QString, QVariant>& t) noexcept {
+  debug.noquote().nospace()
+    << xDBGBlue << "[QMap : " << xDBGReset << t.map.count() << xDBGBlue
+    << "]" << xDBGReset;
+
+  for (auto iter = t.map.constBegin(); iter != t.map.constEnd();
+    ++iter) {
+    __XDBGVARIANT__ variant(iter.value());
+    const auto line =
+      QString("\n  " xDBGBlue "%1" xDBGYellow "%2" xDBGReset
+              " : " xDBGGreen "%3" xDBGReset)
+        .arg(variant.typeName().leftJustified(16, ' '),
+          iter.key().rightJustified(30, ' '),
+          variant.valueString());
+    debug << line;
+  }
+  return debug.space();
+}
+
+#define xDBGMap(__map__) __XDBGMAP__(__map__)
+
+struct __XDBGMETAPROPERTYLIST__ {
+  QObject* object;
+  const int offset, count;
+  inline QMap<int, QMetaProperty> properties() const {
+    auto obj = object->metaObject();
+    QMap<int, QMetaProperty> result;
+    for (int i = offset; i < count; ++i)
+      result.insert(i, obj->property(i));
+    return result;
+  }
+  __XDBGMETAPROPERTYLIST__(QObject* object)
+    : object(object)
+    , offset(object->metaObject()->propertyOffset())
+    , count(object->metaObject()->propertyCount()) {}
+};
+inline QDebug operator<<(
+  QDebug debug, const __XDBGMETAPROPERTYLIST__& x) noexcept {
+  debug.noquote().nospace()
+    << xDBGBlue << "[QProperty:" << xDBGYellow << x.offset << xDBGReset
+    << " -> " << xDBGYellow << x.count << xDBGBlue << "]" << xDBGReset;
+  const auto props = x.properties();
+  for (auto iter = props.constBegin(); iter != props.constEnd();
+    ++iter) {
+    const QString index_s =
+      QString::number(iter.key()).leftJustified(3, ' ');
+    const QString name_s =
+      QString(iter.value().name()).rightJustified(20, ' ');
+    const __XDBGVARIANT__ vairnat(iter.value().read(x.object));
+    const QString type_s = vairnat.typeName().leftJustified(14, ' ');
+    const QString value_s = vairnat.valueString();
+    QString line = QString("  \n" xDBGBlue "[%0]%1" xDBGYellow
+                           "%2" xDBGBlue " : " xDBGGreen "%3" xDBGReset)
+                     .arg(index_s, type_s, name_s, value_s);
+    debug << line;
+  } // for
+  return debug.space();
+}
+
+#define xDBGQPropertyList __XDBGMETAPROPERTYLIST__(this)
 
 #endif // QOOL_DEBUG_HPP
