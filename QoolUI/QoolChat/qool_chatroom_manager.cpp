fd27ae5d6c003b4b1674762fb28f3319945cf947
xiii_1991 <xiii_1991@163.com>
2025-07-11 11:41:29 +0800

    servermanager done

---
+++ QoolUI/QoolChat/qool_chatroom_manager.cpp
@@ -0,0 +1,49 @@
+#include "qool_chatroom_manager.h"
+
+#include <QMutex>
+#include <QThread>
+
+QOOL_NS_BEGIN
+
+QOOL_SIMPLE_SINGLETON_QT_IMPL(ChatRoomManager)
+
+ChatRoomManager::ChatRoomManager()
+  : QObject { nullptr }
+  , m_serverThread { new QThread(this) } {
+  connect(this, &ChatRoomManager::serverPurgingRequested, this,
+    &ChatRoomManager::purgeClosedServers, Qt::QueuedConnection);
+
+  m_serverThread->start();
+}
+
+ChatRoomManager::~ChatRoomManager() {
+  const QStringList names = m_servers.keys();
+  for (const auto& name : names)
+    m_servers.take(name)->deleteLater();
+  m_serverThread->wait();
+  m_serverThread->deleteLater();
+}
+
+QPointer<ChatRoomServer> ChatRoomManager::server(const QString& name) {
+  QPointer<ChatRoomServer> server;
+  if (m_servers.contains(name) && ! m_servers[name].isNull())
+    server = m_servers[name];
+  else {
+    QMutexLocker locker(&m_mutex);
+    server = new ChatRoomServer(name);
+    server->moveToThread(m_serverThread);
+    m_servers[name] = server;
+  }
+  emit serverPurgingRequested();
+  return server;
+}
+
+void ChatRoomManager::purgeClosedServers() {
+  QMutexLocker locker(&m_mutex);
+  const auto names = m_servers.keys();
+  for (const auto& name : names)
+    if (m_servers[name].isNull())
+      m_servers.remove(name);
+}
+
+QOOL_NS_END
